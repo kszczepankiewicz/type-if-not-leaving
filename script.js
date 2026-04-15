199e744f44a763d230dcee573c63afabe2113621
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-04-15 15:44:22 +0200

    feat(index.html): change inputTime type to time from text

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+const constantText = `Type shouldn't you be leaving around `;
+
 const timeInput = document.getElementById('time-input');
 const timeForm = document.getElementById('time-form');
 const result = document.getElementById('result');
 
 timeForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    result.textContent = timeInput.value;
-    timeInput.value = '';
+    renderResult();
 });
+
+
+function renderResult() {
+    const li = document.createElement('li');
+    li.textContent = constantText + timeInput.value;
+    result.append(li);
+    timeInput.value = '';
+}
 
 // const deleteFromHistory = () => {}
 // const chooseFromHistory = () => { }
