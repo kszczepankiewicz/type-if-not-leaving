db95b6fafca9c60605a8e2a38f706fd24b508d32
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-04-15 16:09:57 +0200

    feat: add time by word generation

--- script.js
+++ script.js
@@ -3,23 +3,28 @@ const constantText = `Please type shouldn't you be leaving for Korzeniowskiego s
 const timeInput = document.getElementById('time-input');
 const timeForm = document.getElementById('time-form');
 const result = document.getElementById('result');
+const timeInputDigits = document.getElementById('time-input-digits');
 
 timeForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    renderResult(constantText, timeInput.value);
+    renderResult(constantText, timeInput.value, timeInputDigits.value);
 });
 
 
-function renderResult(text, time) {
-    const span = document.createElement('span');
-    span.textContent = time;
+function renderResult(text, time, timeDigits) {
     const li = document.createElement('li');
     li.textContent = text;
-    li.append(span);
+    li.append(createSpan(timeDigits), ' ', createSpan(time));
     result.append(li);
     // timeInput.value = '';
 }
 
+
+function createSpan(time) {
+    const span = document.createElement('span');
+    span.textContent = time;
+    return span;
+}
 // const deleteFromHistory = () => {}
 // const chooseFromHistory = () => { }
 // const save = message = {}
